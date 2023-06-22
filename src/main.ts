edaf3c8e859111ee0c518ed757de2a46c72a1bc1
SANJUAN\samyak.jain <samyak.jain@avalara.com>
2023-06-22 11:27:05 +0530

    Add swagger and update readme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Identity Reconciliation')
+    .setDescription('The identity reconciliation assignment')
+    .setVersion('1.0')
+    .addTag('identity')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
